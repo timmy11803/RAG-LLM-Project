845737e55b7f9e4e3d75289e1ae7520492324e41
timmy11803 <betancot@gmail.com>
2024-05-02 15:24:50 -0700

    added pdf reader to app

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
 
 def main():
     load_dotenv()
-    st.set_page_config("Ask your PDF ")
+    st.set_page_config(page_title="Chat GPTimmy")
+    st.header("Chat GPTimmy 💻")
+
+    #uploading file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    #extracting the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        st.write(text)
+
+
+
 
 if __name__ == '__main__':
     main()
-
